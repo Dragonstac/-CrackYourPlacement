61d6a5e439e0d0083d5f303d1467c172ad70e778
Shreyash Srivastava <shreyashsriv16@gmail.com>
2024-07-29 23:56:08 +0530

    day 3

---
+++ Questions/majority element.py
@@ -0,0 +1,10 @@
+def majorityElement(self, nums: List[int]) -> int:
+        ans = None
+        count = 0
+
+        for i in nums:
+          if count == 0:
+            ans = i
+          count += (1 if i == ans else -1)
+
+        return ans
